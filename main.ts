73746e96a83feeacfbece21ffba377d76a2c9355
lucasj6 <133249765+lucasj6@users.noreply.github.com>
2023-05-11 10:07:22 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . # # .
+        # # # . .
+        # # # # .
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Ghost)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        # . # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        # . # # .
+        # # # . .
+        # # # # .
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Ghost)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Ghost)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
